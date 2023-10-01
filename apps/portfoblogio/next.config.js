15b4fceecc737289b567893fde5908cb891a4e41
Nguyễn Hữu Nghĩa <nhnghia.dev@gmail.com>
2023-10-01 09:30:30 +0700

    chore: migrate to nx 16.9.1 & add bundle analyzer for portfoblogio

--- apps/portfoblogio/next.config.js
+++ apps/portfoblogio/next.config.js
@@ -2,6 +2,9 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.RUN_ANALYZE === 'true',
+});
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -17,6 +20,7 @@ const nextConfig = {
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
+  withBundleAnalyzer,
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
