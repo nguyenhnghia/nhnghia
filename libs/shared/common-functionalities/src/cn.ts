607d6c30fdb358031c1b9b7cb04af253892f08d3
Nguyễn Hữu Nghĩa <nhnghia.dev@gmail.com>
2023-10-01 10:47:52 +0700

    feat: add common-functionalities lib

---
+++ libs/shared/common-functionalities/src/cn.ts
@@ -0,0 +1,14 @@
+import clsx, { ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * get class names
+ * @param inputs class list
+ * @returns class names
+ */
+function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(inputs));
+}
+
+export { cn };
+export default cn;
