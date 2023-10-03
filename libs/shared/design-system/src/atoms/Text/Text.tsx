9262b1cd72a300c2d550d28e772454825c076d9e
Nguyễn Hữu Nghĩa <nhnghia.dev@gmail.com>
2023-10-03 20:50:25 +0700

    chore(libs/design-system): basic implement for text

--- libs/shared/design-system/src/atoms/Text/Text.tsx
+++ libs/shared/design-system/src/atoms/Text/Text.tsx
@@ -1,18 +1,20 @@
-import { FC, HTMLAttributes } from 'react';
+import { FC } from 'react';
 
-interface TextProps extends HTMLAttributes<HTMLDivElement> {
-  type: 'h1' | 'h2' | 'h3' | 'paragraph' | 'text';
-}
+import Heading1, { Heading1Props } from './Heading1';
+import { CommonTextProps } from './types';
 
-const Text: FC<TextProps> = (props) => {
+type TextProps = CommonTextProps<HTMLSpanElement>;
+
+const Text: FC<TextProps> & { H1: FC<Heading1Props> } = (props) => {
   // props
   const { children } = props;
 
   // render
-  return <div className="">{children}</div>;
+  return <span>{children}</span>;
 };
 
 Text.displayName = 'Text';
+Text.H1 = Heading1;
 
 export { Text };
 export type { TextProps };
---
+++ libs/shared/design-system/src/atoms/Text/types.ts
@@ -0,0 +1,3 @@
+import { HTMLAttributes } from 'react';
+
+export type CommonTextProps<T> = HTMLAttributes<T>;
