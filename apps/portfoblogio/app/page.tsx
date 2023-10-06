00ec716c0e99d1d108ce825ace347b24e1a431aa
Nguyễn Hữu Nghĩa <nhnghia.dev@gmail.com>
2023-10-06 17:36:17 +0700

    chore: add direction icons

--- apps/portfoblogio/app/page.tsx
+++ apps/portfoblogio/app/page.tsx
@@ -1,9 +1,53 @@
-import { Home } from '@nhnghia/static-icons';
+import {
+  ArrowBottomLeft,
+  ArrowBottomRight,
+  ArrowDown,
+  ArrowLeft,
+  ArrowLoop,
+  ArrowRight,
+  ArrowTopLeft,
+  ArrowTopRight,
+  ArrowUp,
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  ChevronUp,
+  DoubleArrowLoop,
+  DoubleChevronDown,
+  DoubleChevronLeft,
+  DoubleChevronRight,
+  DoubleChevronUp,
+  MiniChevronDown,
+  MiniChevronLeft,
+  MiniChevronRight,
+  MiniChevronUp,
+} from '@nhnghia/static-icons';
 
 export default async function Index() {
   return (
-    <div className="text-blue-500">
-      <Home />
+    <div className="flex gap-2 [&>*]:shrink-0 [&>*]:w-8 [&>*]:h-8 text-blue-500">
+      <ChevronDown />
+      <ChevronLeft />
+      <ChevronRight />
+      <ChevronUp />
+      <DoubleChevronDown />
+      <DoubleChevronLeft />
+      <DoubleChevronRight />
+      <DoubleChevronUp />
+      <MiniChevronDown />
+      <MiniChevronLeft />
+      <MiniChevronRight />
+      <MiniChevronUp />
+      <ArrowDown />
+      <ArrowLeft />
+      <ArrowRight />
+      <ArrowUp />
+      <ArrowTopLeft />
+      <ArrowTopRight />
+      <ArrowBottomLeft />
+      <ArrowBottomRight />
+      <ArrowLoop />
+      <DoubleArrowLoop />
     </div>
   );
 }
