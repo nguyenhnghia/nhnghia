a7906bc83c0018cdf95d6fd2e214ad159eab0bda
Nguyễn Hữu Nghĩa <nhnghia.dev@gmail.com>
2023-11-13 10:15:06 +0700

    chore: update prettier config

---
+++ apps/portfoblogio/prettier.config.js
@@ -0,0 +1,9 @@
+const workspaceConfig = require('../../prettier.config');
+
+/** @type {import("prettier").Config} */
+const config = {
+  ...workspaceConfig,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+module.exports = config;
