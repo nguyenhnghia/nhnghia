60eb6fbf0f56be1bd7df82643c6ffcd793ccb817
Nguyễn Hữu Nghĩa <nhnghia.dev@gmail.com>
2024-02-18 21:45:06 +0700

    chore: fix typing

--- packages/web-styling/src/common-types.ts
+++ packages/web-styling/src/common-types.ts
@@ -1,6 +1,6 @@
 export type Screen = "base" | "tablet" | "desktop";
 
-export type ResponsiveUIVariants<V> = V & {
+export type ResponsiveUIVariants<V extends Record<string, unknown>> = V & {
   __responsive?: {
     tablet?: V;
     desktop?: V;
