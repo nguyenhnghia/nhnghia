f4c4a60b1321d8a279b2886bc744751dc505ed46
Nguyễn Hữu Nghĩa <nhnghia.dev@gmail.com>
2024-02-25 19:05:23 +0700

    chore: renaming things

--- apps/portfoblogio/src/app/test/page.tsx
+++ apps/portfoblogio/src/app/test/page.tsx
@@ -1,10 +1,15 @@
+import SvgArrowRight from "@repo/icons/arrow-right";
 import Link from "next/link";
+import { buttonC } from "@components/button-c.css";
 
 export default function TestPage(): JSX.Element {
   return (
     <main>
       <div>
         <Link href="/">go home</Link>
+        <button type="button" className={buttonC.root}>
+          <SvgArrowRight className={buttonC.icon} />
+        </button>
       </div>
     </main>
   );
