942d7c945dea23cdd3cfa3d9fb8f66e40e609ea8
Nguyễn Hữu Nghĩa <nhnghia.dev@gmail.com>
2025-03-04 10:58:11 +0700

    chore: test share

--- apps/portfoblogio/src/app/page.tsx
+++ apps/portfoblogio/src/app/page.tsx
@@ -6,6 +6,15 @@ const Header = dynamic(() => import("@components/header/header"));
 
 export const metadata: Metadata = {
   title: "nhnghia - portfoblogio",
+  openGraph: {
+    type: "article",
+    url: "https://nhnghia-portfoblogio.vercel.app/",
+    title: "Tiêm phòng cúm và phế cầu cùng lúc có an toàn không? - Dev test",
+    description: "Những lưu ý quan trọng khi tiêm (dev test)",
+  },
+  other: {
+    "fb:app_id": "1768713483442649",
+  },
 };
 
 const RootPage: React.FC = () => {
